2f5846b5314a3f4fa62be78c8fc08ef7bf3b24ed
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2022-03-11 17:18:32 +0000

    create channels example

--- go-channels/channels.go
+++ go-channels/channels.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	// To create a new channel (buffer = 2) called messages
+	messages := make(chan string, 2)
+
+	// Create a goroutine and send a message to the channel
+	go func() { messages <- "hello from goroutine 1" }()
+
+	// Create a second goroutine
+	go func() { messages <- "hello from goroutine 2" }()
+
+	// Get the message from the channel
+	fmt.Println(<-messages)
+	fmt.Println(<-messages)
 }
